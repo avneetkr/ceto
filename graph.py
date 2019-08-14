5dd30e1cfa2efe84a8d8999c183fb9d0f5967713
avneetkr <avneeet06@gmail.com>
2019-08-14 13:43:56 -0700

    Created map.py and graph.py

---
+++ graph.py
@@ -0,0 +1,22 @@
+'''
+Graph class
+'''
+
+class Graph(object):
+    def __init__(start_node=None):
+        self.start = start_node
+        self.nodes = []
+        self.edges = [] #change?
+        #self.weight - how to represent weights?
+
+    def add_node():
+        #add a new node to graph
+
+    def add_edge():
+        #add a new edge to graph
+
+    def printGraph():
+        #print all nodes and edges
+        
+    def get_weight():
+        #return weight given edge (may change?)
---
+++ map.py
@@ -0,0 +1,16 @@
+'''
+map.py:
+- take a maze from a text file
+- input it into a 2D array
+- Read the 2D array to create a Graph with nodes, edges and edge weights (are all weights = 1?)
+- Implement A* search on the graph
+'''
+import Graph
+
+def read_maze(filename=None):
+    maze_array = []
+
+def make_graph(maze_array):
+    maze_graph = {}
+
+def a_star_search(maze_graph):
